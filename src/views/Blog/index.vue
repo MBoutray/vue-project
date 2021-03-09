42a7383ab8001f766848691793091efebd78a784
TheMadSandman <martindeboutray@hotmail.fr>
2021-03-09 18:20:08 +0100

    Created views files

---
+++ src/views/Blog/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <post-list></post-list>
+  <pagination></pagination>
+</template>
+
+<script>
+import Pagination from "@/components/Post/Pagination";
+import PostList from "@/components/Post/PostList";
+
+export default {
+  name: "index",
+  components: {
+    PostList,
+    Pagination
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Bienvenue sur mon blog !</h1>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
